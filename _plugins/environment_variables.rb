1e0d00183cb5e24620bcc535c8fb80495f16f89c
Francisco Guzman <fguzman@qf.org.qa>
2015-10-21 01:16:12 +0300

    corrected problems with references to site.deploy

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -5,6 +5,11 @@ module Jekyll
     def generate(site)
       site.config['env'] = ENV['JEKYLL_ENV'] || 'github'
       # Add other environment variables to `site.config` here...
+      if  (site.config['env'] == 'github')
+        site.config['deploy']="https://guzmanhe.github.io" 
+      else 
+        site.config['deploy']="http://alt.qcri.org/~guzmanhe/"
+      end
     end
 
   end
